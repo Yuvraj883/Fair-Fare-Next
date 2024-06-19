33694e84d5e53ba35b507f27a5ffbf41a4a6d972
Yuvraj <devyuvraj883@gmail.com>
2024-06-19 15:39:10 +0530

    Updated style

--- components/Home/SearchSection.jsx
+++ components/Home/SearchSection.jsx
@@ -3,7 +3,8 @@ import Input from "./Input";
 const SearchSection = ()=>{
   return(
     <>
-    <div className="px-8 bg-gray-200 py-4 h-full">
+    <div className="px-8  py-4 shadow-lg bg-gray-100">
+      <h2 className="font-semibold text-lg ">Get a ride</h2>
     <Input pickup={true}/>
     <Input pickup={false}/>
     <button className="w-full hover:text-green-300 bg-black text-white font-bold text-xl py-2 rounded-lg">Search</button>
